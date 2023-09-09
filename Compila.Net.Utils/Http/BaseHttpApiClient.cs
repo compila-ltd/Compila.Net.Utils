3ae0f0796afb015ebe2e89e51f677c76d5172d3a
eyonys <eyonys@gmail.com>
2023-09-09 15:06:03 -0400

    BaseRequest

--- Compila.Net.Utils/Http/BaseHttpApiClient.cs
+++ Compila.Net.Utils/Http/BaseHttpApiClient.cs
@@ -5,43 +5,43 @@ using RestSharp;
 
 namespace Compila.Net.Utils.Http
 {
-    public abstract class BaseHttpApiClient
-    {
-        private readonly RestClient restClient;
+	public abstract class BaseHttpApiClient
+	{
+		private readonly RestClient restClient;
 
-        public BaseHttpApiClient(IEndpointData endpointData, bool ignoreSsl = false)
-        {
-            var options = new RestClientOptions
-            {
-                RemoteCertificateValidationCallback = default,
-                BaseUrl = new Uri(endpointData.BaseUrl)
-            };
+		public BaseHttpApiClient(IEndpointData endpointData, bool ignoreSsl = false)
+		{
+			var options = new RestClientOptions
+			{
+				RemoteCertificateValidationCallback = default,
+				BaseUrl = new Uri(endpointData.BaseUrl)
+			};
 
-            if (ignoreSsl)
-                options.RemoteCertificateValidationCallback = (sender, certificate, chain, sslPolicyErrors) => true;
+			if (ignoreSsl)
+				options.RemoteCertificateValidationCallback = (sender, certificate, chain, sslPolicyErrors) => true;
 
-            restClient = new RestClient(options);
-        }
+			restClient = new RestClient(options);
+		}
 
-        protected async Task<RestResponse<T>> ExecuteAsync<T>(RestRequest request) where T : new()
-        {
-            request = TransformHeaders(request);
-            var response = await restClient.ExecuteAsync<T>(request);
+		protected async Task<RestResponse<T>> ExecuteAsync<T>(BaseRequest request) where T : new()
+		{
+			request = TransformHeaders(request);
+			var response = await restClient.ExecuteAsync<T>(request.RestRequest);
 
-            return response;
-        }
+			return response;
+		}
 
-        protected async Task<RestResponse> ExecuteAsync(RestRequest request)
-        {
-            request = TransformHeaders(request);
-            var response = await restClient.ExecuteAsync(request);
+		protected async Task<RestResponse> ExecuteAsync(BaseRequest request)
+		{
+			request = TransformHeaders(request);
+			var response = await restClient.ExecuteAsync(request.RestRequest);
 
-            return response;
-        }
+			return response;
+		}
 
-        protected virtual RestRequest TransformHeaders(RestRequest request)
-        {
-            return request;
-        }
-    }
+		protected virtual BaseRequest TransformHeaders(BaseRequest request)
+		{
+			return request;
+		}
+	}
 }
---
+++ Compila.Net.Utils/Http/BaseRequest.cs
@@ -0,0 +1,14 @@
+﻿using RestSharp;
+
+namespace Compila.Net.Utils.Http
+{
+	public class BaseRequest
+	{
+		public RestRequest RestRequest { get; set; }
+
+		public BaseRequest(RestRequest restRequest)
+		{
+			RestRequest = restRequest;
+		}
+	}
+}
