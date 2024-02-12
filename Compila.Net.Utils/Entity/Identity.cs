26f2813d86f52de690979b61a27377bc3a3a2aeb
eyonys <eyonys@gmail.com>
2024-02-12 15:43:24 -0500

    Identity type

---
+++ Compila.Net.Utils/Entity/Identity.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Compila.Net.Utils.Entity
+{
+    public abstract class Identity : IEquatable<Identity>, IIdentity
+    {
+        public Identity()
+        {
+            this.Id = Guid.NewGuid().ToString();
+        }
+
+        public Identity(string id)
+        {
+            this.Id = id;
+        }
+
+        public string Id { get; set; }
+
+        public bool Equals(Identity id)
+        {
+            if (object.ReferenceEquals(this, id)) return true;
+            if (object.ReferenceEquals(null, id)) return false;
+            return this.Id.Equals(id.Id);
+        }
+
+        public override bool Equals(object anotherObject)
+        {
+            return Equals(anotherObject as Identity ?? throw new Exception("Object to compare is not of Identity type."));
+        }
+
+        public override int GetHashCode()
+        {
+            return (this.GetType().GetHashCode() * 907) + this.Id.GetHashCode();
+        }
+
+        public override string ToString()
+        {
+            return this.GetType().Name + " [Id=" + Id + "]";
+        }
+    }
+
+    public interface IIdentity
+    {
+        string Id { get; set; }
+    }
+}
