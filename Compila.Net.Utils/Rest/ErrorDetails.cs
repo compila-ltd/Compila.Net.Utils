f52f0a46e8fd661306830eefe190ae071799ac0f
eyonys <eyonys@gmail.com>
2024-02-21 12:16:38 -0500

    Added ErrorDetails class

---
+++ Compila.Net.Utils/Rest/ErrorDetails.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace Compila.Net.Utils.Rest
+{
+    public class ErrorDetails
+    {
+        [JsonPropertyName("statusCode")]
+        public int StatusCode { get; set; }
+        [JsonPropertyName("message")]
+        public string? Message { get; set; }
+        [JsonPropertyName("errors")]
+        public List<string> Errors { get; set; } = new List<string>();
+
+        public override string ToString() => JsonSerializer.Serialize(this);
+    }
+}
