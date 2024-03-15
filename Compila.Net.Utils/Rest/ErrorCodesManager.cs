58e3cbf7eb3a4ad2607d7bbbf09996d5d2edb8bf
eyonys <eyonys@gmail.com>
2024-03-15 12:52:03 -0400

    Error codes manager

---
+++ Compila.Net.Utils/Rest/ErrorCodesManager.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+
+namespace Compila.Net.Utils.Rest
+{
+    public abstract class ErrorCodes
+    {
+        public ErrorDictionary AuthenticationErrors { get; set; } = new ErrorDictionary();
+
+        public ErrorCodes()
+        {
+            GenerateAuthenticationErrors();
+        }
+
+        void GenerateAuthenticationErrors()
+        {
+            AuthenticationErrors = new ErrorDictionary
+            {
+                { "E400001", "invalid_credentials" },
+                { "E400002", "email_not_confirmed" },
+                { "E400003", "duplicated_email" },
+                { "E400004", "weak_password" }
+            };
+        }
+    }
+
+    public class ErrorDictionary : Dictionary<string, string>
+    {
+        public string GetError(ErrorDetailsInResponse errorDetails)
+        {
+            return GetError(errorDetails.ErrorCode ?? "");
+        }
+
+        public string GetError(string errorCode)
+        {
+            if (this.ContainsKey(errorCode))
+            {
+                return this[errorCode];
+            }
+            else
+            {
+                return "unknown_error";
+            }
+        }
+    }
+}
---
+++ Compila.Net.Utils/Rest/ErrorDetailsInResponse.cs
@@ -0,0 +1,17 @@
+﻿using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace Compila.Net.Utils.Rest
+{
+    public class ErrorDetailsInResponse
+    {
+        [JsonPropertyName("statusCode")]
+        public int StatusCode { get; set; }
+        [JsonPropertyName("message")]
+        public string? Message { get; set; }
+        [JsonPropertyName("errorCode")]
+        public string? ErrorCode { get; set; }
+
+        public override string ToString() => JsonSerializer.Serialize(this);
+    }
+}
