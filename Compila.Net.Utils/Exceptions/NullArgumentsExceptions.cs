52672cdde555c22f421686a0870c4364144b1ce1
eyonys <eyonys@gmail.com>
2024-03-21 00:16:16 -0400

    Exceptions

---
+++ Compila.Net.Utils/Exceptions/NullArgumentsExceptions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Compila.Net.Utils.Exceptions
+{
+    public class NullArgumentsException : ArgumentNullException
+    {
+        public NullArgumentsException(string paramName) : base(paramName: paramName, $"{paramName} can't be null.") { }
+    }
+
+    public class InternalNullArgumentException : ArgumentNullException
+    {
+        bool _isInternal = true;
+        public InternalNullArgumentException(string message, bool isInternal = true) : base(message: message, null)
+        {
+            _isInternal = isInternal;
+        }
+    }
+}
