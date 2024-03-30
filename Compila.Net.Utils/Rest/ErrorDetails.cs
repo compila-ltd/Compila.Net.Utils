79cbf5255287c0c230d2210372e6beda854781d2
eyonys <eyonys@gmail.com>
2024-03-30 01:34:57 -0400

    Error details refactoring.

---
+++ Compila.Net.Utils/Rest/ErrorDetails.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace Compila.Net.Utils.Rest
+{
+	public abstract class ErrorDetailsBase
+	{
+		[JsonPropertyName("statusCode")]
+		public int StatusCode { get; set; }
+		[JsonPropertyName("message")]
+		public string Message { get; set; }
+
+		public ErrorDetailsBase(string message, int statusCode)
+		{
+			Message = message;
+			StatusCode = statusCode;
+		}
+
+		public override string ToString() => JsonSerializer.Serialize(this);
+	}
+
+	public class ErrorDetails : ErrorDetailsBase
+	{
+		public ErrorDetails(string message, int statusCode) : base(message, statusCode) { }
+	}
+
+	public class ErrorDetailsList : ErrorDetailsBase
+	{
+		[JsonPropertyName("errors")]
+		public List<string> Errors { get; set; } = new List<string>();
+
+		public ErrorDetailsList(string message, int statusCode) : base(message, statusCode) { }
+	}
+
+	public class ErrorDetailsInResponse : ErrorDetailsBase
+	{
+		[JsonPropertyName("errorCode")]
+		public string? ErrorCode { get; set; }
+
+		public ErrorDetailsInResponse(string message, int statusCode) : base(message, statusCode) { }
+	}
+}
