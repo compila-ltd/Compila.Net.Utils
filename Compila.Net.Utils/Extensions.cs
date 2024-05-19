34ccadd620330d0cf6aa4a9633ef28863e07a5fd
eyonys <eyonys@gmail.com>
2024-05-19 10:18:12 -0400

    Extensions helpers

--- Compila.Net.Utils/Extensions.cs
+++ Compila.Net.Utils/Extensions.cs
@@ -3,26 +3,43 @@
 namespace Compila.Net.Utils
 {
 	public static class Extensions
-    {
-        public static decimal TruncateDecimal(this decimal @this, int places)
-        {
-            var multipler = (int)Math.Pow(10, places);
-            return decimal.Truncate(@this * multipler) / multipler;
-        }
+	{
+		public static decimal TruncateDecimal(this decimal @this, int places, bool parseDecimals = false)
+		{
+			var multipler = (int)Math.Pow(10, places);
+			var truncated = decimal.Truncate(@this * multipler) / multipler;
 
-        public static int DecimalPlaces(this decimal @this)
-        {
-            return BitConverter.GetBytes(decimal.GetBits(@this)[3])[2];
-        }
+			if (parseDecimals)
+				return decimal.Parse(truncated.ToString($"F{places}"));
 
-        public static string Base64Encode(this string @this)
-        {
-            return Convert.ToBase64String(Encoding.UTF8.GetBytes(@this));
-        }
+			return truncated;
+		}
 
-        public static string Base64Decode(this string @this)
-        {
-            return Encoding.UTF8.GetString(Convert.FromBase64String(@this));
-        }
-    }
+		public static int DecimalPlaces(this decimal @this)
+		{
+			return BitConverter.GetBytes(decimal.GetBits(@this)[3])[2];
+		}
+
+		public static string Base64Encode(this string @this)
+		{
+			return Convert.ToBase64String(Encoding.UTF8.GetBytes(@this));
+		}
+
+		public static string Base64Decode(this string @this)
+		{
+			return Encoding.UTF8.GetString(Convert.FromBase64String(@this));
+		}
+
+		public static string TruncateString(this string @this, int maxLength)
+		{
+			if (@this.Length <= maxLength)
+				return @this;
+
+			var middlePoint = maxLength / 2;
+			string start = @this[..middlePoint];
+			string end = @this[^middlePoint..];
+
+			return $"{start}...{end}";
+		}
+	}
 }
--- Compila.Net.Utils/RandomStringGenerator.cs
+++ Compila.Net.Utils/RandomStringGenerator.cs
@@ -4,30 +4,53 @@ using System.Text;
 namespace Compila.Net.Utils
 {
 	public static class RandomStringGenerator
-    {
-        public static string GenerateRandomToken(int length)
-        {
-            char[] chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789".ToCharArray();
+	{
+		public static string GenerateRandomToken(int length, RandomStringType randomStringType = RandomStringType.AlphaNumeric)
+		{
+			char[] chars = randomStringType switch
+			{
+				RandomStringType.AlphaNumeric => "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789".ToCharArray(),
+				RandomStringType.LowerCaseAlphaNumeric => "abcdefghijklmnopqrstuvwxyz0123456789".ToCharArray(),
+				RandomStringType.UpperCaseAlphaNumeric => "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789".ToCharArray(),
+				RandomStringType.Alpha => "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ".ToCharArray(),
+				RandomStringType.LowerCaseAlpha => "abcdefghijklmnopqrstuvwxyz".ToCharArray(),
+				RandomStringType.UpperCaseAlpha => "ABCDEFGHIJKLMNOPQRSTUVWXYZ".ToCharArray(),
+				RandomStringType.Numeric => "0123456789".ToCharArray(),
+				RandomStringType.Hexadecimal => "0123456789ABCDEF".ToCharArray(),
+				_ => "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789".ToCharArray(),
+			};
 
-            byte[] data = new byte[4 * length];
+			byte[] data = new byte[4 * length];
 
-            using (RandomNumberGenerator crypto = RandomNumberGenerator.Create())
-            {
-                crypto.GetBytes(data);
-            }
+			using (RandomNumberGenerator crypto = RandomNumberGenerator.Create())
+			{
+				crypto.GetBytes(data);
+			}
 
-            StringBuilder result = new StringBuilder(length);
-            for (int i = 0; i < length; i++)
-            {
-                var rnd = BitConverter.ToUInt32(data, i * 4);
-                var idx = rnd % chars.Length;
+			StringBuilder result = new StringBuilder(length);
+			for (int i = 0; i < length; i++)
+			{
+				var rnd = BitConverter.ToUInt32(data, i * 4);
+				var idx = rnd % chars.Length;
 
-                result.Append(chars[idx]);
-            }
+				result.Append(chars[idx]);
+			}
 
-            var token = result.ToString();
+			var token = result.ToString();
 
-            return token;
-        }
-    }
+			return token;
+		}
+	}
+
+	public enum RandomStringType
+	{
+		AlphaNumeric,
+		LowerCaseAlphaNumeric,
+		UpperCaseAlphaNumeric,
+		Alpha,
+		LowerCaseAlpha,
+		UpperCaseAlpha,
+		Numeric,
+		Hexadecimal
+	}
 }
