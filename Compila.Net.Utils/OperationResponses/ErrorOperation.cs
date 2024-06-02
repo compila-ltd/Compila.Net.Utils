8c7e8b7ada9e41af336935d883b2af2e627cc85b
eyonys <eyonys@gmail.com>
2024-06-02 09:31:42 -0400

    Operation responses

---
+++ Compila.Net.Utils/OperationResponses/ErrorOperation.cs
@@ -0,0 +1,12 @@
+﻿namespace Compila.Net.Utils.OperationResponses
+{
+    public class ErrorOperation : OperationBaseResponse
+    {
+        public string ErrorMessage { get; set; }
+
+        public ErrorOperation(string errorMessage) : base(false)
+        {
+            ErrorMessage = errorMessage;
+        }
+    }
+}
---
+++ Compila.Net.Utils/OperationResponses/OperationBaseResponse.cs
@@ -0,0 +1,9 @@
+﻿namespace Compila.Net.Utils.OperationResponses
+{
+    public class OperationBaseResponse
+    {
+        public bool Success { get; set; }
+
+        protected OperationBaseResponse(bool success) => Success = success;
+    }
+}
