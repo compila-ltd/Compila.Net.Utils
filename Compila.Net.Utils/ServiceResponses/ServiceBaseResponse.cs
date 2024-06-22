6259a66779286de8dff05eab02a8e8e65d6eeced
eyonys <eyonys@gmail.com>
2024-06-22 01:50:44 -0400

    Removed Service Responses

--- Compila.Net.Utils/ServiceResponses/ServiceBaseResponse.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Compila.Net.Utils.ServiceResponses
-{
-	public abstract class ServiceBaseResponse
-	{
-		public bool Success { get; set; }
-
-		protected ServiceBaseResponse(bool success) => Success = success;
-	}
-}
