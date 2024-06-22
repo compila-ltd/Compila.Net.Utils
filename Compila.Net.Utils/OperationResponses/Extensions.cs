c75c3c8ca2532c40786f4093c33c3fb081b9a59f
eyonys <eyonys@gmail.com>
2024-06-22 01:52:37 -0400

    Operation Responses improvements

---
+++ Compila.Net.Utils/OperationResponses/Extensions.cs
@@ -0,0 +1,15 @@
+﻿namespace Compila.Net.Utils.OperationResponses
+{
+    public static class OperationBaseResponseExtensions
+    {
+        public static TResultType GetResult<TResultType>(this OperationBaseResponse response)
+        {
+            if (response is SuccessfulOperationResponse<TResultType> okResponse)
+            {
+                return okResponse.Result;
+            }
+
+            throw new InvalidOperationException($"Response is not of type SuccessfulOperation<{nameof(TResultType)}>");
+        }
+    }
+}
---
+++ Compila.Net.Utils/OperationResponses/SuccessfulOperationResponse.cs
@@ -0,0 +1,14 @@
+﻿namespace Compila.Net.Utils.OperationResponses
+{
+    public class SuccessfulOperationResponse : OperationBaseResponse
+    {
+        public SuccessfulOperationResponse() : base(true) { }
+    }
+
+    public class SuccessfulOperationResponse<TResult> : OperationBaseResponse
+    {
+        public TResult Result { get; set; }
+
+        public SuccessfulOperationResponse(TResult result) : base(true) => Result = result;
+    }
+}
