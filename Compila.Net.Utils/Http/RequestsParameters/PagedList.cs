01acb0589b26ddc6de3d9625b84c84cf8a24485d
Eyonys Gonzalez Marcaida <eyonys88@gmail.com>
2026-01-10 15:05:06 -0500

    Add async paging to PagedList; deprecate sync method
    Introduce ToPagedListAsync for async paging with delegates.
    Mark ToPagedList (sync) as obsolete and hide from editor.
    Add System.ComponentModel using for attribute support.

--- Compila.Net.Utils/Http/RequestsParameters/PagedList.cs
+++ Compila.Net.Utils/Http/RequestsParameters/PagedList.cs
@@ -1,6 +1,8 @@
-﻿namespace Compila.Net.Utils.Http.RequestsParameters
+﻿using System.ComponentModel;
+
+namespace Compila.Net.Utils.Http.RequestsParameters
 {
-	public class PagedList<T> : List<T>
+    public class PagedList<T> : List<T>
     {
         public RequestMetadata Metadata { get; set; }
 
@@ -17,6 +19,14 @@
             AddRange(items);
         }
 
+        /// <summary>
+        /// Deprecated: synchronous enumeration-based paging will be removed in a future release.
+        /// Use <see cref="ToPagedListAsync(IQueryable{T}, int, int, Func{IQueryable{T}, Task{int}}, Func{IQueryable{T}, Task{List{T}}})"/>
+        /// or perform paging on the caller using async query methods (for example EF Core's async methods).
+        /// </summary>
+        [Obsolete("ToPagedList(IEnumerable<T>...) is deprecated and will be removed in a future release. " +
+                  "Use ToPagedListAsync(IQueryable<T>, ...) or perform paging with async queries instead.", false)]
+        [EditorBrowsable(EditorBrowsableState.Never)]
         public static PagedList<T> ToPagedList(IEnumerable<T> source, int pageNumber, int pageSize)
         {
             var count = source.Count();
@@ -24,6 +34,16 @@
                 .Skip((pageNumber - 1) * pageSize)
                 .Take(pageSize)
                 .ToList();
+
+            return new PagedList<T>(items, count, pageSize, pageNumber);
+        }
+
+        public static async Task<PagedList<T>> ToPagedListAsync(IQueryable<T> source, int pageNumber, int pageSize, Func<IQueryable<T>,
+            Task<int>> countFunc, Func<IQueryable<T>, Task<List<T>>> listFunc)
+        {
+            var count = await countFunc(source);
+
+            var items = await listFunc(source.Skip((pageNumber - 1) * pageSize).Take(pageSize));
 
             return new PagedList<T>(items, count, pageSize, pageNumber);
         }
